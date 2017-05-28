a85ac8da5a7ca2a3b8d848f4f8847bde0b343154
sarjumulmi <mulmi.sarju@gmail.com>
2017-05-28 12:29:47 -0400

    display menu method extended to loop through user input. working bin file created.

--- lib/dslr_shop/cli.rb
+++ lib/dslr_shop/cli.rb
@@ -23,6 +23,10 @@ class DslrShop::CLI
   end
 
   def list_cameras
+    puts "*******************************************************".green
+    puts "              Welcome to the DSLR Shop".red
+    puts "  Your one stop shop for the most popular DSLR cameras".red
+    puts "*******************************************************".green
     puts "        List of the most popular cameras     ".blue
     puts "*******************************************************".green
     DslrShop::Camera.all.each.with_index(1) {|camera, index|
@@ -32,13 +36,43 @@ class DslrShop::CLI
   end
 
   def display_menu
-    puts "*******************************************************".green
-    puts "              Welcome to the DSLR Shop".red
-    puts "  Your one stop shop for the most popular DSLR cameras".red
-    puts "*******************************************************".green
-    self.list_cameras
-    puts "Which camera number are you interested in today?"
-    puts "Please enter 1 ~ #{DslrShop::Camera.all.size}."
+    camera_list = DslrShop::Camera.all
+    list_size = camera_list.size
+    menu_array = (1..list_size).to_a
+    menu_array.concat(["LIST", "list", "EXIT", "exit", "MENU", "menu"])
+    menu_array = menu_array.map(&:to_s)
+    puts "What would you like to do?".blue
+    puts "Please enter 1 ~ #{list_size} to view a specific camera number!!!".blue
+    puts "Please enter LIST to view the camera list again!!!".blue
+    puts "Please enter EXIT to exit!!!".blue
+    puts "Please enter MENU to view the menu again!!!".blue
+    puts "*********************************************************************************************".green
+    index = gets.strip
+    until menu_array.include?(index)
+      puts "Invalid Entry!!!!!!".red
+      puts "Please Enter the camera number, LIST, EXIT or MENU".red
+      puts "*********************************************************************************************".green
+      index = gets.strip
+    end
+
+    if index.to_i>0
+      puts "#{index}. #{camera_list[index.to_i - 1].brand.upcase} #{camera_list[index.to_i - 1].name}"
+    elsif index == ("LIST" || "list")
+      self.list_cameras
+    elsif index == ("EXIT" || "exit")
+      self.goodbye
+    else
+      self.display_menu
+    end
+    self.display_menu
+  end
+
+  def goodbye
+    puts "THANK YOU FOR VISITING THE DSLR SHOP!!!!".blue
+    puts "HOPE TO SEE YOU AGAIN!!!!".blue
+    puts "HAVE A GREAT DAY!!!".blue
+    puts "*********************************************************************************************".green
+    exit
   end
 
 end
